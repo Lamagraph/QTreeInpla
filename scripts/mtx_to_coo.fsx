78383826d945e0868695234a1c6c5b7eb26b19a5
Danil-Zaripov <catharsiandev@gmail.com>
2026-03-02 19:07:45 +0300

    Add experiments

--- scripts/mtx_to_coo.fsx
+++ scripts/mtx_to_coo.fsx
@@ -1,22 +1,25 @@
 #!/usr/bin/env -S dotnet fsi
 
+// mtx to Inpla coo script
+// The input matrix should not contain any other data(like comments)
+
 open System.IO
 open System
 
 let cooExtension = ".coo"
 
-let getBigCoo (linewords: string array array) = 
+let getBigCoo (linewords: string array array) =
     linewords
     |> Array.map (fun x -> sprintf "(%s, %s, 1)" x.[0] x.[1])
     |> String.concat ", "
-    |> sprintf "[%s]" 
+    |> sprintf "[%s]"
 
 let handleFile path =
     let lines = File.ReadLines(path) |> Seq.toArray
     let linewords = lines |> Array.map (fun s -> s.Split " ")
     let first = linewords.[0]
-    
-    let nrows, ncols, nnz = 
+
+    let nrows, ncols, nnz =
         int first.[0], int first.[1], int first.[2]
 
     let tl = Array.skip 1 linewords
@@ -27,10 +30,10 @@ let handleFile path =
 
 
 let main (args: string array) =
-    if Array.length args = 1 then 
+    if Array.length args = 1 then
         eprintfn "Path to mtx matrix was not provided"
         1
-    else 
+    else
         let path = args[1]
         handleFile path
         0
